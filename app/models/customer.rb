5421cd12bade2829d21642ac37e7e5f80c7e8023
Carlo Casorzo <carloc@pagoranking.com>
2015-02-02 03:04:51 +0000

    Improved products UI

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ActiveRecord::Base
   has_many :products, through: :orders
   
   #scope :debtors, -> { where paid: false }
+  
+  
+  def total_debt
+    self.orders.unpaid.inject(0.0) { |sum, order| sum += order.total_cost  }
+  end
+  
+  def total_income
+    self.orders.paid.inject(0.0) { |sum, order| sum += order.total_cost  }
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :customers, through: :orders
+  
+  def total_sales
+    self.orders.inject(0) { |sum, order| sum += order.quantity  }
+  end
+  
+  def total_income
+    self.orders.paid.inject(0) { |sum, order| sum += order.total_cost  }
+  end
+  
+  def total_debt
+    self.orders.unpaid.inject(0) { |sum, order| sum += order.total_cost  }
+  end
 end
